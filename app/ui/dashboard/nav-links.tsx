baeb8edf768205ec412b0ff106f07efc2305e95a
citywwc <citywwc@163.com>
2024-04-30 16:35:51 +0800

    prettier format

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,10 +1,10 @@
-'use client'
+'use client';
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
-import Link from 'next/link'
+import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 // Map of links to display in the side navigation.
